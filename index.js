6f8f979eb90bdf47a0688908157bbe59570febad
GeraldCenteno <geraldcenteno@student.olympic.edu>
2024-06-07 20:48:08 -0700

    still troubleshooting index.js

--- index.js
+++ index.js
@@ -2,31 +2,14 @@ import { createServer } from 'http';   //create an http server
 import { readFile } from 'fs';       //create a file handler to read files
 import { join } from 'path';   //create a path resolver
 
+//server set to listen on port 5500
+createServer.listen(5500, () => {
+    console.log('Server is listening on http://localhost:5500');
+});
+
 createServer((req, res) => {
     let url = req.url;
 
-    if(url === '/')
-        url = 'index.html';
-    else if(url === 'level0')
-        url = 'level0.html';
-    else if(url === 'level1')
-        url = 'level1.html';
-    else if(url === 'level2')
-        url = 'level2.html';
-    else if(url === 'level3')
-        url = 'level3.html';
-    else if(url === 'level4')
-        url = 'level4.html';
-    else if(url === 'level5')
-        url = 'level5.html';
-    else if(url === 'thelaw')
-        url = 'thelaw.html';
-    else if(url === 'opnav')
-        url = 'opnav.html';
-    else
-        url = 'error.html';
-
-    /*
     //switch to resolve common pagename to filename
     switch(url){
         case '/':
@@ -59,7 +42,7 @@ createServer((req, res) => {
         default:
             url = 'error.html';
             break;
-    }*/
+    }
 
     const filePath = join(__dirname, url);
 
@@ -82,7 +65,3 @@ createServer((req, res) => {
     });
 })
 
-//server set to listen on port 5500
-.listen(5500, () => {
-    console.log('Server is listening on http://localhost:5500');
-});
